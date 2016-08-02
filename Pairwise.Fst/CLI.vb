cf93b32ce777731e8d6bd009a88e99f7555a758c
xieguigang <xie.guigang@gmail.com>
2016-08-03 06:57:32 +0800

    fix configs

--- Pairwise.Fst/CLI.vb
+++ Pairwise.Fst/CLI.vb
@@ -1,3 +1,10 @@
-﻿Module CLI
+﻿Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 
+Module CLI
+
+    <ExportAPI("/fst")>
+    Public Function fst(args As CommandLine) As Integer
+
+    End Function
 End Module
