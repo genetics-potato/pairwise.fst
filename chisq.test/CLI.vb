7b5c8c27104eedd3e16af9ad3f580b2ded4e9d2f
xieguigang <genetics@smrucc.org>
2016-08-04 04:30:50 +0800

    batch chisq.test task tools

--- chisq.test/CLI.vb
+++ chisq.test/CLI.vb
@@ -1,6 +1,7 @@
 ﻿Imports Microsoft.VisualBasic.CommandLine
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.DocumentFormat.Csv
+Imports Microsoft.VisualBasic.Language.UnixBash
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports Pairwise.Fst
 
@@ -15,5 +16,21 @@ Module CLI
         Dim result = TestMatrix.chisqTest(genotypes).ToArray
         Call result.MatrixView(genotypes, [in].BaseName).Save(out, Encodings.ASCII)
         Return result.GetJson.SaveTo(out.TrimSuffix & ".json").CLICode
+    End Function
+
+    <ExportAPI("/snp.genotype.chisq.test.batch",
+               Usage:="/snp.genotype.chisq.test.batch /in <in.DIR> [/out <out.Csv>]")>
+    Public Function Batch_chisqTest(args As CommandLine) As Integer
+        Dim [in] As String = args("/in")
+        Dim EXPORT As String =
+            args.GetValue("/out", [in].TrimSuffix & ".snp.genotype_chisq.test/")
+        Dim cmd As String = GetType(CLI).API(NameOf(SNP_genotypeChisqTest))
+
+        For Each snp As String In ls - l - r - wildcards("*.csv") <= [in]
+            Dim out As String = EXPORT & snp.BaseName & ".json"
+            Call App.SelfFolk($"{cmd} /in {snp.CliPath} /out {out.CliPath}")
+        Next
+
+        Return 0
     End Function
 End Module
