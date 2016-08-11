350b91ab569fa23e03ec8df6b6e8a2f978db43ab
xieguigang <genetics@smrucc.org>
2016-08-12 07:57:02 +0800

    innatedb_curated_genes.xls table parser

---
+++ InnateDB/innatedb_curated_genes.vb
@@ -0,0 +1,23 @@
+﻿Imports Microsoft.VisualBasic.DocumentFormat.Csv.StorageProvider.Reflection
+Imports Microsoft.VisualBasic.Serialization.JSON
+
+''' <summary>
+''' ``innatedb_curated_genes.xls``
+''' </summary>
+Public Class innatedb_curated_genes
+
+    <Column("InnateDB Gene ID")>
+    Public Property InnateDBGeneID As String
+
+    <Column("Gene Symbol")>
+    Public Property GeneSymbol As String
+    Public Property Species As String
+
+    <Column("PubMED ID")>
+    Public Property PubMEDID As String
+    Public Property Annotation As String
+
+    Public Overrides Function ToString() As String
+        Return Me.GetJson
+    End Function
+End Class
